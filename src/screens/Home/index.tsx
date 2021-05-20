6b595022d3b43e969e0fa3338a3b1f394dd3f1d7
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2021-05-20 10:36:48 -0300

    feat(Dashboard): add onRefresh to Scroller

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { RefreshControl } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import {
@@ -16,38 +17,54 @@ import BankAccountWidget from "../../components/BankAccountWidget";
 
 export default function Home() {
     const [hideSensitive, setHideSensitive] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
 
     const toggleSensitive = () => setHideSensitive(!hideSensitive);
 
+    function timeout(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    const onRefresh = async () => {
+        setRefreshing(true);
+        await timeout(2000);
+        setRefreshing(false);
+    };
+
     return (
         <Container>
-            <TopBar>
-                <WelcomeArea>
-                    <WelcomeText>Olá, Mauri</WelcomeText>
-                </WelcomeArea>
-                <IconsArea>
-                    <HideSensitiveButton onPress={toggleSensitive}>
-                        <Ionicons
-                            name={
-                                hideSensitive
-                                    ? "eye-off-outline"
-                                    : "eye-outline"
-                            }
-                            size={28}
-                            color="#FFFFFF"
-                        />
-                    </HideSensitiveButton>
-                    <SettingsButton>
-                        <Ionicons
-                            name="settings-outline"
-                            size={28}
-                            color="#FFFFFF"
-                        />
-                    </SettingsButton>
-                </IconsArea>
-            </TopBar>
-
-            <Scroller>
+            <Scroller
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+            >
+                <TopBar>
+                    <WelcomeArea>
+                        <WelcomeText>Olá, Mauri</WelcomeText>
+                    </WelcomeArea>
+                    <IconsArea>
+                        <HideSensitiveButton onPress={toggleSensitive}>
+                            <Ionicons
+                                name={
+                                    hideSensitive
+                                        ? "eye-off-outline"
+                                        : "eye-outline"
+                                }
+                                size={28}
+                                color="#FFFFFF"
+                            />
+                        </HideSensitiveButton>
+                        <SettingsButton>
+                            <Ionicons
+                                name="settings-outline"
+                                size={28}
+                                color="#FFFFFF"
+                            />
+                        </SettingsButton>
+                    </IconsArea>
+                </TopBar>
                 <CreditCardWidget />
                 <BankAccountWidget />
             </Scroller>
